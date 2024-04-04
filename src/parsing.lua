89dbb239f908ed03ca65681e38852e34b8ab819b
Conor C. Peterson <conorpetersondev@gmail.com>
2024-04-04 14:02:07 +0200

    feat(parsing): add parse_dns_packet function

--- src/parsing.lua
+++ src/parsing.lua
@@ -1,6 +1,7 @@
 -- Import modules
 local DNS      = require "src.dns"
 local Decoding = require "src.decoding"
+local Reader   = require "src.modules.reader"
 
 -- Define module variable
 local Parsing  = {}
@@ -41,6 +42,43 @@ function Parsing.parse_record(reader)
 
   -- Return a DNS Record object based on the decoded/unpacked values
   return DNS.Record(decoded_name, type, class, ttl, data)
+end
+
+--- Parse DNS packet binary data
+-- @param data - DNS packet byte data
+-- @return DNS.Packet
+function Parsing.parse_dns_packet(data)
+  -- Initialize reader object with the data
+  local reader = Reader.new(data)
+
+  -- Parse the header
+  local header = Parsing.parse_header(reader)
+
+  -- Parse the questions
+  local questions = {}
+  for i = 1, header.num_questions do
+    questions[i] = Parsing.parse_question(reader)
+  end
+
+  -- Parse the answers
+  local answers = {}
+  for i = 1, header.num_answers do
+    answers[i] = Parsing.parse_record(reader)
+  end
+
+  -- Parse the authorities
+  local authorities = {}
+  for i = 1, header.num_authorities do
+    authorities[i] = Parsing.parse_record(reader)
+  end
+
+  -- Parse the additionals
+  local additionals = {}
+  for i = 1, header.num_additionals do
+    additionals[i] = Parsing.parse_record(reader)
+  end
+
+  return DNS.Packet(header, questions, answers, authorities, additionals)
 end
 
 return Parsing
